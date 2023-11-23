dbb9e6cd72372b93a56d46f1b8cbb5c3b3c8ffab
Charbelmnc <108475657+Charbelmnc@users.noreply.github.com>
2023-11-23 12:14:58 +0100

    mon dernier commit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -23,6 +23,9 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
+  
+    
+    
     /**
      * The application's route middleware groups.
      *
@@ -64,5 +67,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'checkRoomCount' => \App\Http\Middleware\CheckRoomCountBeforeDelete::class,
+
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('rooms', RoomController::class);
+    Route::delete('rooms/{room}', 'RoomController@destroy')->name('rooms.destroy');
 });
 Route::middleware(['auth'])->group(function () {
     Route::resource('hotels', HotelController::class);
